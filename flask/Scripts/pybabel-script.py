d3fd9a327e8e2fa2dc65461d2483fa99a2e80dfd
Crimsoneer <andrea.varotsis@gmail.com>
2014-03-06 14:52:04 +0000

    first milestone - Hello World test

---
+++ flask/Scripts/pybabel-script.py
@@ -0,0 +1,10 @@
+#!"F:\Users\Andreas\Documents\Dropbox\Projects\Thinkful Python\Unit 3\Flask Mega Tutorial\flask\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'Babel==1.3','console_scripts','pybabel'
+__requires__ = 'Babel==1.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Babel==1.3', 'console_scripts', 'pybabel')()
+    )
